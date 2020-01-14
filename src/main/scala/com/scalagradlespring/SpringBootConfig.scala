661e89b421e64fd417b600a811ca483589aab563
Oneide Luiz Schneider <oneidels@gmail.com>
2020-01-14 19:00:17 -0300

    first commit

---
+++ src/main/scala/com/scalagradlespring/SpringBootConfig.scala
@@ -0,0 +1,9 @@
+package com.scalagradlespring
+
+import org.springframework.boot.autoconfigure.{EnableAutoConfiguration, SpringBootApplication}
+
+@EnableAutoConfiguration
+@SpringBootApplication
+class SpringBootConfig {
+
+}
